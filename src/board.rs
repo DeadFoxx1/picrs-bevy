85098fd1f10e6026f11befb7ec958f0b384840c1
DeadFoxx1 <206004751+DeadFoxx1@users.noreply.github.com>
2026-01-02 12:23:49 -0600

    add room for top hints

--- src/board.rs
+++ src/board.rs
@@ -13,11 +13,15 @@ impl Plugin for BoardPlugin {
     }
 }
 pub const GRID_BG_COLOR: [f32; 3] = [0., 0., 0.];
+
 pub const TOP_HINTS_PERCENT: f32 = 0.25;
 pub const TOP_MARGIN_PERCENT: f32 = 0.05; //5% of the screen's height per margin
 pub const SIDE_MARGIN_RATIO: (f32, f32) = (9., 1.); //9:1
+
 //in the case that screen width < screen height
 pub const MIN_SIZE_OF_SQUARE_PERCENT: f32 = 0.70; //70% of the screens width
 
 pub const CELL_FG_COLOR: [f32; 3] = [1., 1., 1.];
-pub const BORDER_TO_CELL_FG_RATIO: (f32, f32) = (1., 20.); //1:6
+pub const BORDER_TO_CELL_FG_RATIO: (f32, f32) = (1., 20.); //1:20
+
+pub const HINT_BG_COLOR: [f32; 3] = [1., 1., 0.];
--- src/board/bg.rs
+++ src/board/bg.rs
@@ -1,5 +1,5 @@
 use super::{
-    GRID_BG_COLOR, MIN_SIZE_OF_SQUARE_PERCENT, SIDE_MARGIN_RATIO, TOP_HINTS_PERCENT,
+    GRID_BG_COLOR, HINT_BG_COLOR, MIN_SIZE_OF_SQUARE_PERCENT, SIDE_MARGIN_RATIO, TOP_HINTS_PERCENT,
     TOP_MARGIN_PERCENT,
 };
 use bevy::{prelude::*, window::WindowResized};
@@ -8,12 +8,15 @@ pub struct BgPlugin;
 impl Plugin for BgPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, draw_board_bg)
-            .add_systems(Update, update_board_bg);
+            .add_systems(Update, update_bg);
     }
 }
 
 #[derive(Component)]
 pub struct GridBg;
+
+#[derive(Component)]
+pub struct TopHintBg;
 
 pub fn draw_board_bg(
     mut commands: Commands,
@@ -25,29 +28,41 @@ pub fn draw_board_bg(
         Mesh2d(mesh.add(Rectangle::default())),
         MeshMaterial2d(material.add(Color::srgb_from_array(GRID_BG_COLOR))),
     ));
+    commands.spawn((
+        TopHintBg,
+        Mesh2d(mesh.add(Rectangle::default())),
+        MeshMaterial2d(material.add(Color::srgb_from_array(HINT_BG_COLOR))),
+    ));
 }
 
-fn update_board_bg(
-    mut bg: Single<&mut Transform, With<GridBg>>,
+fn update_bg(
+    mut grid_bg: Single<&mut Transform, (With<GridBg>, Without<TopHintBg>)>,
+    mut top_hint_bg: Single<&mut Transform, (With<TopHintBg>, Without<GridBg>)>,
     mut resized_events: MessageReader<WindowResized>,
 ) {
     for event in resized_events.read() {
         let window_width = event.width;
         let window_height = event.height;
         let top_margin = window_height * TOP_MARGIN_PERCENT;
-        let top_hint = window_height * TOP_HINTS_PERCENT;
-        let board_size = f32::min(
-            window_height - (2. * top_margin) - top_hint,
+        let grid_size = f32::min(
+            window_height - (2. * top_margin),
             window_width * MIN_SIZE_OF_SQUARE_PERCENT,
         );
-        let left_margin = ((window_width - board_size) * SIDE_MARGIN_RATIO.0)
+        let top_hint = grid_size * TOP_HINTS_PERCENT;
+        let grid_size = grid_size - top_hint;
+        let left_margin = ((window_width - grid_size) * SIDE_MARGIN_RATIO.0)
             / (SIDE_MARGIN_RATIO.0 + SIDE_MARGIN_RATIO.1);
         let top_of_screen = window_height / 2.;
         let left_of_screen = -(window_width / 2.);
 
-        bg.translation.x = left_of_screen + left_margin + (board_size / 2.);
-        bg.translation.y = top_of_screen - top_hint - top_margin - (board_size / 2.);
-        bg.scale.x = board_size;
-        bg.scale.y = board_size;
+        grid_bg.translation.x = left_of_screen + left_margin + (grid_size / 2.);
+        grid_bg.translation.y = top_of_screen - top_hint - top_margin - (grid_size / 2.);
+        grid_bg.scale.x = grid_size;
+        grid_bg.scale.y = grid_size;
+
+        top_hint_bg.translation.x = grid_bg.translation.x;
+        top_hint_bg.translation.y = grid_bg.translation.y + (grid_size / 2.) + (top_hint / 2.);
+        top_hint_bg.scale.x = grid_bg.scale.x;
+        top_hint_bg.scale.y = top_hint;
     }
 }
