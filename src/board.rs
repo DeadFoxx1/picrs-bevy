5654201a6ec4fbef9bcefbbdee57103e22394893
DeadFoxx1 <206004751+DeadFoxx1@users.noreply.github.com>
2026-01-02 22:48:40 -0600

    make hint cells for top hints

--- src/board.rs
+++ src/board.rs
@@ -5,11 +5,12 @@ pub mod hints;
 use bevy::prelude::*;
 use bg::BgPlugin;
 use cells::CellsPlugin;
+use hints::HintsPlugin;
 
 pub struct BoardPlugin;
 impl Plugin for BoardPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((BgPlugin, CellsPlugin));
+        app.add_plugins((BgPlugin, CellsPlugin, HintsPlugin));
     }
 }
 pub const GRID_BG_COLOR: [f32; 3] = [0., 0., 0.];
@@ -17,9 +18,12 @@ pub const GRID_BG_COLOR: [f32; 3] = [0., 0., 0.];
 pub const HINTS_PERCENT: f32 = 0.25;
 pub const VERT_MARGIN_PERCENT: f32 = 0.05; //5% of the screen's height per margin
 pub const HORI_MARGIN_PERCENT: f32 = 0.30;
-pub const SIDE_MARGIN_RATIO: (f32, f32) = (1., 1.); //9:1
+pub const SIDE_MARGIN_RATIO: (f32, f32) = (9., 1.); //9:1
 
 pub const CELL_FG_COLOR: [f32; 3] = [1., 1., 1.];
-pub const BORDER_TO_CELL_FG_RATIO: (f32, f32) = (1., 20.); //1:20
+pub const BORDER_TO_CELL_FG_RATIO: (f32, f32) = (1., 10.); //1:20
 
-pub const HINT_BG_COLOR: [f32; 3] = [1., 1., 0.];
+pub const HINTS_FG_COLOR: [f32; 3] = [1., 1., 1.];
+pub const BORDER_TO_HINTS_FG_RATIO: (f32, f32) = (1., 10.); //1:20
+
+pub const HINT_BG_COLOR: [f32; 3] = [0., 0., 0.];
--- src/board/hints.rs
+++ src/board/hints.rs
@@ -1,10 +1,11 @@
 pub mod top_hints;
 
 use bevy::prelude::*;
+use top_hints::TopHintsPlugin;
 
 pub struct HintsPlugin;
 impl Plugin for HintsPlugin{
     fn build(&self, app: &mut App) {
-        todo!();
+        app.add_plugins(TopHintsPlugin);
     }
 }
