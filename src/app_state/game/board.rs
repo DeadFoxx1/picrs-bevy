010e77ee55cac90fd0289a2a595a0a6e3ead57b3
DeadFoxx1 <206004751+DeadFoxx1@users.noreply.github.com>
2026-01-25 13:43:31 -0600

    update colors

--- src/app_state/game/board.rs
+++ src/app_state/game/board.rs
@@ -13,7 +13,7 @@ impl Plugin for BoardPlugin {
         app.add_plugins((BgPlugin, CellsPlugin, HintsPlugin));
     }
 }
-pub const GRID_BG_COLOR: [f32; 3] = [0., 0., 0.];
+pub const GRID_BG_COLOR: [f32; 3] = [0.3, 0.31, 0.39];
 
 pub const HINTS_PERCENT: f32 = 0.25;
 
@@ -24,11 +24,11 @@ pub const VERT_MARGIN_RATIO: (f32, f32) = (1., 1.); //9:1
 
 pub const CELL_EMPTY_COLOR: [f32; 3] = [1., 1., 1.];
 pub const CELL_FILLED_COLOR: [f32; 3] = [0.12, 0.12, 0.18];
-pub const CELL_CROSSED_COLOR: [f32; 3] = [1., 0., 0.];
-pub const CELL_SOLVED_COLOR: [f32; 3] = [0., 1., 0.];
+pub const CELL_CROSSED_COLOR: [f32; 3] = [1., 0.22, 0.26];
+pub const CELL_SOLVED_COLOR: [f32; 3] = [0.4, 1., 0.5];
 pub const BORDER_TO_CELL_FG_RATIO: (f32, f32) = (1., 10.); //1:20
 
 pub const HINTS_FG_COLOR: [f32; 3] = [1., 1., 1.];
 pub const BORDER_TO_HINTS_FG_RATIO: (f32, f32) = (1., 10.); //1:20
 
-pub const HINT_BG_COLOR: [f32; 3] = [0., 0., 0.];
+pub const HINT_BG_COLOR: [f32; 3] = [0.3, 0.31, 0.39];
